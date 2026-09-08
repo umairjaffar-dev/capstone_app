36c2fb39dfa046c04609de09730255083b90c8b4
Umair Jaffar <umair.jaffar@betacodespk.com>
2026-09-08 17:34:59 +0500

    Add post creation and deletion functionality
    - Implemented `createPost` and `deletePost` functions in the post controller.
    - Added validation for post creation using Zod and improved error handling.
    - Updated routes to include endpoints for creating and deleting posts.
    - Introduced a utility function for formatting Zod validation errors.
    - Added `node-pg-migrate` dependency for database migrations.

--- src/routes/posts.route.ts
+++ src/routes/posts.route.ts
@@ -1,8 +1,14 @@
 import { Router, Request, Response } from "express";
-import { getUserAllPosts } from "../controllers/post.controller";
+import {
+  createPost,
+  deletePost,
+  getUserAllPosts,
+} from "../controllers/post.controller";
 
 const postRouter = Router();
 
 postRouter.get("/posts", getUserAllPosts);
+postRouter.post("/posts", createPost);
+postRouter.delete("/posts/:id/", deletePost); // Get id from url path in controller.
 
 export default postRouter;
--- src/validations/post.validation.ts
+++ src/validations/post.validation.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 export const CreatePostSchema = z.object({
   user_id: z.number().int().positive("user_id must be a positive number"),
-  title: z.string().min(1, "Title is required").max(200, "Title too long"),
+  title: z.string().min(1, "title is required").max(200, "title too long"),
   content: z.string().optional(),
 });
-
-export type CreatePostSchemaType = z.infer<typeof CreatePostSchema>
+export type CreatePostSchemaType = z.infer<typeof CreatePostSchema>;
